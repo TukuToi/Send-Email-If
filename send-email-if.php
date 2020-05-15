af56bb55d92d80e2baa3d09c1c2a086def233e5a
Beda Schmid <arbeit@yoonect.com>
2020-05-16 00:20:00 +0700

    Initial Commit

---
+++ send-email-if.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Plugin Name: Send Email If
+ * Description: Send Emails on conditions
+ * Plugin URI: https://www.tukutoi.com
+ * Author: TukuToi
+ * Author URI: https://www.tukutoi.com
+ * Version: 1.0.0
+ * License: GPL2
+ * Text Domain: tkt-send-email-if
+ * Domain Path: domain/path
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+add_action( 'the_content', 'tkt_sei_get_queried_object' );
+
+function tkt_sei_get_queried_object() {
+	
+	global $wp;
+
+	$str = add_query_arg( $wp->query_vars, home_url( $wp->request ) );
+	$req = substr($str, strpos($str, "?") + 1);
+	$params = array();
+
+	foreach (explode('&', $req) as $chunk) {
+	    $param = explode("=", $chunk);
+	    if ($param) {
+	        $params[$param[0]] = isset($param[1]) ? $param[1] : '';
+	    }
+	}
+
+	$current_data['visitor'] = wp_get_current_user();
+	$current_data['query'] = $params; 
+
+	tkt_sei_settings($params, $current_data);
+
+}
+
+function tkt_sei_settings($params, $current_data){
+
+	//This could be a GUI
+	$settings = array('sample-page', 'monthnum'=>'05');
+
+	if (array_intersect($settings, $params)) {
+		tkt_sei_send_mail($current_data);
+	}
+
+}
+
+function tkt_sei_send_mail($current_data){
+
+	//This could be a GUI
+	$add = array('Cc'=>'an@email.com', 'Cc' => 'another@email.com');
+	$subject = "Subject Message";
+
+	$the_visitor = $current_data['visitor']->ID !== 0 ? $current_data['visitor']->data->user_login : 'undefined user';
+	$message = 'Hello, '. $the_visitor .' just visited your profile.';
+
+ 	$headers = array();
+	foreach ($add as $key => $email) {
+		$headers[] = $key.': '.$email;
+	}
+
+	$user_to_send_mail = get_user_by('login',$current_data['query']['name']);
+
+	if (is_object($user_to_send_mail)) 
+		wp_mail( $user_to_send_mail->data->user_email, $subject, $message, $headers );
+	
+}
