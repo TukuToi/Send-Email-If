915f8a1f9220b854f547dd72328e643d5945c472
Beda Schmid <arbeit@yoonect.com>
2020-05-22 23:13:27 +0700

    Elevate abstraction level a little

--- send-email-if.php
+++ send-email-if.php
@@ -1,73 +1,98 @@
 <?php
-
 /**
  * Plugin Name: Send Email If
- * Description: Send Emails on conditions
- * Plugin URI: https://www.tukutoi.com
+ * Description: Send Emails to X when Y happens. This version is specifically adapted for https://juniorcollegiatetour.com: Emails will be sent to the address stored in the "parents email" Field on subpages, only when a logged in Coach is viewing the subpage (player profile). 
+ * Plugin URI: https://www.tukutoi.com/program/send-email-if/
  * Author: TukuToi
- * Author URI: https://www.tukutoi.com
- * Version: 1.0.0
+ * Author URI: https://www.tukutoi.com/author/Beda_Schmid/
+ * Version: 1.5.0
  * License: GPL2
  * Text Domain: tkt-send-email-if
- * Domain Path: domain/path
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-add_action( 'wp_footer', 'tkt_sei_get_queried_object' );
+$role = '';
 
+/**
+ * Register a Backend Metabox for adding email, on pages only
+ */
+function tkt_sei_register_meta_boxes() {
+    add_meta_box( 'tkt_sei_email_address', __( 'Email Address', 'tkt-send-email-if' ), 'tkt_sei_display_callback', 'post' );
+}
+
+/**
+ * Populate the metabox
+ */
+function tkt_sei_display_callback( $post ) {
+    include plugin_dir_path( __FILE__ ) . './metabox.php';
+}
+
+/**
+ * Save content of meta box to database
+ */
+function tkt_sei_save_meta_box( $post_id ) {
+
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+
+    if ( $parent_id = wp_is_post_revision( $post_id ) ) {
+        $post_id = $parent_id;
+    }
+
+    $fields = [
+        'tkt_sei_email_address',
+    ];
+
+    foreach ( $fields as $field ) {
+        if ( array_key_exists( $field, $_POST ) ) {
+            update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
+        }
+    }
+}
+
+/**
+ * Front end, get current page (player), and current user (coach)
+ */
 function tkt_sei_get_queried_object() {
-	
-	global $wp;
+	//WP Object
+	global $post;
 
-	$str = add_query_arg( $wp->query_vars, home_url( $wp->request ) );
-	$req = substr($str, strpos($str, "?") + 1);
-	$params = array();
-
-	foreach (explode('&', $req) as $chunk) {
-	    $param = explode("=", $chunk);
-	    if ($param) {
-	        $params[$param[0]] = isset($param[1]) ? $param[1] : '';
-	    }
+	if ( is_single() && 'post' == get_post_type() ) {
+		//we are on a single post 
+		if ( get_option('permalink_structure') ) {
+			//we use permalinks
+			$current_user = wp_get_current_user();
+			if ( in_array( $role, (array) $current_user->roles ) ) {
+				//it's a Coach!
+				$parent_email = get_post_meta($post->ID, 'tkt_sei_email_address', true);
+				if ($parent_email) {
+					//an email is stored
+					$content = 'Hello, '.$current_user->first_name.' '.$current_user->last_name.' has just visited '.$post->post_title;
+					tkt_sei_send_mail($parent_email, $content);
+				}	
+			}
+		}
 	}
-
-	$current_data['visitor'] = wp_get_current_user();
-	$current_data['query'] = $params; 
-
-	tkt_sei_settings($params, $current_data);
-
 }
 
-function tkt_sei_settings($params, $current_data){
+/**
+ * Send the Email
+ * @param  [string] $receiver [email of receiver]
+ * @param  [string] $content  [content of email]
+ */
+function tkt_sei_send_mail($receiver, $content){
 
-	//This could be a GUI
-	$settings = array('sample-page', 'monthnum'=>'05', 'beda_schmid');
-
-	if (!empty(array_intersect($settings, $params))) {
-		tkt_sei_send_mail($current_data);
-	}
-
+	$subject = "Visit detected!";
+	
+	wp_mail( $receiver, $subject, $content);
 }
 
-function tkt_sei_send_mail($current_data){
-
-	//This could be a GUI
-	$add = array('Cc'=>'an@email.com', 'Cc' => 'another@email.com');
-	$subject = "Subject Message";
-
-	$the_visitor = $current_data['visitor']->ID !== 0 ? $current_data['visitor']->data->user_login : 'undefined user';
-	$message = 'Hello, '. $the_visitor .' just visited your profile.';
-
- 	$headers = array();
-	foreach ($add as $key => $email) {
-		$headers[] = $key.': '.$email;
-	}
-
-	$username = isset($current_data['query']['name']) ? $current_data['query']['name'] : (isset($current_data['query']['pagename']) ? $current_data['query']['pagename'] : '');
-	$user_to_send_mail = get_user_by('login', $username);
-	
-
-	if (is_object($user_to_send_mail)) 
-		wp_mail( $user_to_send_mail->data->user_email, $subject, $message, $headers );
-	
-}
+/**
+ * Bootstrap everything
+ */
+//Add metaboxes
+add_action( 'add_meta_boxes', 'tkt_sei_register_meta_boxes' );
+//Save metaboxes when saving post
+add_action( 'save_post', 'tkt_sei_save_meta_box' );
+//Front End Event (Visit and email)
+add_action( 'wp_footer', 'tkt_sei_get_queried_object' );
